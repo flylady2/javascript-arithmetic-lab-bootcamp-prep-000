befa3dd94d05cffa275e4a10227e0617b9cd6cf2
Leslie Stevens <flylady@mail.utexas.edu>
2017-11-01 02:34:04 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(x,y) {
+  return x + y
+}
+function subtract(a,b) {
+  return a - b
+}
+function multiply (x,y) {
+  return x * y
+}
+function divide (c, d) {
+  return c/d
+}
+function inc(n) {
+  return n +=1
+}
+function dec(n) {
+  return n -=1
+}
+function makeInt(n) {
+  parseInt('n', 10)
+  return n
+}
+function preserveDecimal(n) {
+  parseFloat('n')
+  return n
+}
